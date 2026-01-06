c03488dacda7110777a4db5a010c2951959ab86e
BuildTools <unconfigured@null.spigotmc.org>
2026-01-07 01:35:58 +0800

    Added ... the program

---
+++ downloads-organizer.py
@@ -0,0 +1,95 @@
+import os
+import shutil
+import mimetypes
+import random
+
+#determine if y contains x, then move to z if true
+#determine if the file type contains a certain string then move to folder if such is true
+def determine(x,y,z):
+    if x in mimetypes.guess_type(y)[0]:
+        shutil.move(y,z)
+        print ("moved", y, "to", z)
+        
+#for file extensions that require special treatment
+#cuntbag shitfaced little stuff like webp yucky
+def exception(x,y,z):
+    if y.endswith(x):
+        shutil.move(y,z)
+        print ("moved", y, "to", z )
+
+#establish society       
+os.chdir('G:\DOWNLOAD')
+downloads=os.path.dirname(os.path.realpath(__file__))
+folders=['IMAGES','FOLDERS','VIDEOS','APPLICATIONS','ARCHIVES','ETC','DOCUMENTS','AUDIO','CSTIMER','TO PASS', 'LTSPICE','DUPLICATES','CIRCUITS']
+exceptionisms=["TO PASS", "downloads organizer.py", "vibranceGUI.exe", "youtube-dl.exe","WFDownloaderApp-BETA-64bit.exe","yt-dlp.exe", "metadataremover.exe","fileoptimizer", "p6"]
+#check if folders actually exist
+for x in folders:
+    if x in os.listdir():
+        print(x, "exists, no need to create new directory")
+    else:
+        print(x, "does not exist, creating directory...\n")
+        os.mkdir(x)
+
+#the juice
+for file in os.listdir():
+    print(file)
+    if file not in folders and file not in exceptionisms:
+        try:
+            # exceptions. fuck these file types (except for .rar)
+            # just kidding, fuck mimetypes for not recognizing these
+            # just kidding, fucking the system only applies when the system is deliberately fucking you
+            # we do live in a society
+            exception(".dll", file, 'ETC')
+            exception(".tar", file, 'ARCHIVES')
+            exception(".7z", file, 'ARCHIVES')
+            exception(".tar.gz", file, 'ARCHIVES')
+            exception(".csv", file, 'DOCUMENTS')
+            exception(".config", file, 'ETC')
+            exception(".circ", file, 'CIRCUITS')
+            exception(".webp", file, 'IMAGES')
+            exception(".rar",file,'ARCHIVES')
+            exception(".msi",file,'APPLICATIONS')
+            exception(".htm",file,'DOCUMENTS')
+            exception(".tex",file,'DOCUMENTS')
+            exception(".doc",file,'DOCUMENTS')
+            exception(".asc",file,'LTSPICE')
+            exception(".op.raw",file,'LTSPICE')
+            exception(".plt",file,'LTSPICE')
+            exception(".raw",file,'LTSPICE')
+            exception(".exe", file, 'APPLICATIONS')
+            exception(".json", file, "ETC") 
+            #is it a directory?
+            if os.path.isdir(file):
+                shutil.move(file,'FOLDERS') 
+            if "cstimer" in file:
+                shutil.move(file,'CSTIMER')
+            #Nonetypes are nonces. 
+            if mimetypes.guess_type(file)[0] !=None:
+                determine('video', file, 'VIDEOS')
+                determine('image', file, 'IMAGES')
+                determine('document', file, 'DOCUMENTS')
+                determine('pdf', file, 'DOCUMENTS')
+                determine('text', file, 'DOCUMENTS')
+                determine('zip', file, 'ARCHIVES')
+                determine('octet', file, 'APPLICATIONS')
+                determine('audio', file, 'AUDIO')
+                
+                
+            #for filetypes i may or may not care about
+            else:
+                shutil.move(file,'ETC')
+        #i genuinely just got lazy so i just said fuck it! if file not found, go eat shit and next!
+        except FileNotFoundError:
+            pass
+        except PermissionError:
+            print(file, "may be open! or you have no permission. Find it out yourself, fool")
+        except shutil.Error:
+            print(file, "may already exist in folder! Find out yourself, fuckface.")
+            x = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
+            shutil.move(file,'DUPLICATES/'+x+' '+file)
+            print(x)
+
+
+while 1==1:
+    input("Enter anything to continue: ")
+    break
